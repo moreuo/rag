c907dfb7d7763bd1a34d73bf75c5a7a5a1a3e1fc
moreuo <98001570+moreuo@users.noreply.github.com>
2025-02-10 16:13:04 -0300

    refactor: rewrite and split functions for better modularity

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
+import logging
 import tempfile
 
 import chromadb
 from git import Repo
 from ollama import Client
+
+logging.basicConfig(level=logging.INFO)
 
 
 def create_response(message, model="deepseek-r1"):
@@ -14,145 +17,91 @@ def create_response(message, model="deepseek-r1"):
     ).message.content
 
 
-def insert_documents(collection, repo_data):
+def clone_repository(repo_url):
+    temp_dir = tempfile.mkdtemp()
+    repo = Repo.clone_from(repo_url, temp_dir)
+    logging.info(f"{repo_url} cloned to {temp_dir}.")
+    return repo
+
+
+def store_in_chromadb(collection, data):
+    count = 0
     existing_ids = set(collection.get()["ids"]) if collection.count() > 0 else set()
-
-    new_docs = [
-        {
-            "id": commit["commit_hash"],
-            "document": commit["message"],
-            "metadata": {
-                "author": commit["author"],
-                "email": commit["email"],
-                "datetime": commit["datetime"],
-                "changed_files": ", ".join(
-                    [
-                        f"{file['file']} ({file['change_type']})"
-                        for file in commit["changed_files"]
-                    ]
-                ),
-            },
-        }
-        for commit in repo_data["documents"]
-        if commit["commit_hash"] not in existing_ids
-    ]
-
-    if new_docs:
-        collection.add(
-            ids=[doc["id"] for doc in new_docs],
-            documents=[doc["document"] for doc in new_docs],
-            metadatas=[doc["metadata"] for doc in new_docs],
-        )
-        print(f"* {len(new_docs)} new commits inserted!")
-    else:
-        print("* No new commits to insert!")
-
-
-def query_collection(collection, query, n_results=5):
-    results = collection.query(
-        query_texts=[query],
-        n_results=n_results,
+    for docs in data:
+        if docs["ids"] not in existing_ids:
+            count += 1
+            collection.add(ids=docs["ids"], documents=docs["documents"])
+    logging.info("No new documents to insert.") if count == 0 else logging.info(
+        f"{count} new documents added to collection."
     )
 
-    if not results["metadatas"]:
-        return None
 
-    return [
-        {
-            "id": results["ids"][i][0],
-            "metadata": results["metadatas"][i][0],
-            "document": results["documents"][i][0] if results["documents"] else None,
-        }
-        for i in range(len(results["metadatas"]))
-    ]
+def query_chromadb(collection, query, n_results=2):
+    results = collection.query(query_texts=[query], n_results=n_results)
+    logging.info(f"Results found for the query '{query}':\n{results}")
+    return results
 
 
-def extract_repo_data(repo_url):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        repo = Repo.clone_from(repo_url, temp_dir)
-
-        repo_info = {
-            "repo_name": repo_url.split("/")[-1],
-            "branches": [branch.name for branch in repo.branches],
-            "tags": [
-                {"name": tag.name, "commit": tag.commit.hexsha} for tag in repo.tags
-            ],
-        }
-
-        commits_data = []
-        for commit in repo.iter_commits():
-            commits_data.append(
+def extract_data(repo):
+    extracted_data = []
+    for commit in repo.iter_commits():
+        commit_data = {
+            "ids": commit.hexsha,
+            "documents": str(
                 {
-                    "commit_hash": commit.hexsha,
                     "author": commit.author.name,
                     "email": commit.author.email,
-                    "message": commit.message.strip(),
                     "datetime": commit.committed_datetime.isoformat(),
-                    "changed_files": [
-                        {"file": diff.a_path, "change_type": diff.change_type}
-                        for diff in commit.diff(commit.parents[0])
-                    ]
-                    if commit.parents
-                    else [],
+                    "message": commit.message,
+                    "diff": ", ".join([diff.a_path for diff in commit.diff(None)]),
+                    "branch": repo.active_branch.name
+                    if not repo.head.is_detached
+                    else "detached",
                 }
-            )
-
-        data = {
-            "id": repo_info["repo_name"],
-            "metadata": {
-                "repo_info": repo_info,
-                "commit_count": len(commits_data),
-            },
-            "documents": commits_data,
+            ),
         }
+        extracted_data.append(commit_data)
 
-        return data
+    logging.info(f"{len(extracted_data)} extracted commits from the repository.")
+    return extracted_data
 
 
 if __name__ == "__main__":
-    chat_history = []
+    # Extract Repository Data
+    url = ""
+    assert url, "Please provide a repository url."
+    repo = clone_repository(url)
 
-    # Chroma Collection
-    chroma_client = chromadb.Client()
+    # Initialize ChromaDB
+    chroma_client = chromadb.PersistentClient(path="chroma.db")
     collection = chroma_client.get_or_create_collection(name="repository")
 
-    # Extract Repository Data
-    repository_url = ""
+    # Load and Insert Documents
+    documents = extract_data(repo)
+    store_in_chromadb(collection, documents)
 
-    if collection.count() == 0:
-        print("* Extracting repository data...")
-        data = extract_repo_data(repository_url)
-        print("* Inserting into the database...")
-        insert_documents(collection, data)
+    # Enrich Chat History
+    chat_history = []
+    assistant = """I am going to ask you a question, which I would like you to answer
+        based only on the provided context, and not any other information.
+        If there is not enough information in the context to answer the question,
+        say "I am not sure", then try to make a guess.
+        Break your answer up into nicely readable paragraphs."""
+    chat_history.append({"role": "assistant", "content": assistant})
 
     # Chat Loop
     while True:
-        query = input("=> Ask Me: ")
+        query = str(input("=> Ask Me: "))
 
         if query.strip() == "":
             print("\nGoodbye!")
             break
 
         # Query Collection
-        relevant_data = query_collection(collection, query)
+        context = query_chromadb(collection, query)
 
-        if not relevant_data:
-            print("\n* Error: No relevant information in the database. *\n")
-            continue
-
-        # Enrich Chat History
-        context = (
-            "To answer the user question, you must use the relevant data below:\n\n"
-        )
-        for data in relevant_data:
-            context += (
-                f"Commit: {data['id']}\n"
-                f"Author: {data['metadata']['author']}\n"
-                f"Date: {data['metadata']['datetime']}\n"
-                f"Message: {data['document']}\n"
-                f"Changed Files: {data['metadata']['changed_files']}\n\n"
-            )
-        chat_history.append({"role": "assistant", "content": context})
+        # Ask Question
+        user = f"The question is '{query}'. Here is all the context you have: {context}"
         chat_history.append({"role": "user", "content": query})
 
         # Create Response
