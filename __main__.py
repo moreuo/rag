c7cc962a6efa7bd9070eed8c26b4f4d5782c1e1d
moreuo <98001570+moreuo@users.noreply.github.com>
2025-03-16 23:06:17 -0300

    feat: add docling and qdrant

--- __main__.py
+++ __main__.py
@@ -1,114 +1,135 @@
 import logging
-import tempfile
 
-import chromadb
 import gradio as gr
-from chromadb.config import Settings
-from git import Repo
+from docling.backend.msword_backend import MsWordDocumentBackend
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
+from docling.chunking import HybridChunker
+from docling.datamodel.base_models import InputFormat
+from docling.document_converter import (
+    DocumentConverter,
+    PdfFormatOption,
+    WordFormatOption,
+)
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
 from ollama import Client
+from qdrant_client import QdrantClient
 
 logging.basicConfig(level=logging.INFO)
 
 
-def create_response(message, model="deepseek-r1"):
-    client = Client()
-
-    return client.chat(
+def ollama_chat(messages, model="llama3.2"):
+    return OLLAMA_CLIENT.chat(
         model=model,
-        messages=message,
+        messages=messages,
     ).message.content
 
 
-def clone_repository(repo_url):
-    temp_dir = tempfile.mkdtemp()
-    repo = Repo.clone_from(repo_url, temp_dir)
-    logging.info(f"{repo_url} cloned to {temp_dir}.")
-    return repo
-
-
-def store_in_chromadb(collection, data):
-    count = 0
-    existing_ids = set(collection.get()["ids"]) if collection.count() > 0 else set()
-    for docs in data:
-        if docs["ids"] not in existing_ids:
-            count += 1
-            collection.add(ids=docs["ids"], documents=docs["documents"])
-    logging.info("No new documents to insert.") if count == 0 else logging.info(
-        f"{count} new documents added to collection."
+def qdrant_store(documents, metadata):
+    QDRANT_CLIENT.add(
+        collection_name=COLLECTION_NAME,
+        documents=documents,
+        metadata=metadata,
+        batch_size=64,
     )
+    logging.info(f"New document added to collection: {documents, metadata}")
 
 
-def query_chromadb(collection, query, n_results=2):
-    results = collection.query(query_texts=[query], n_results=n_results)
-    logging.info(f"Results found for the query '{query}':\n{results}")
+def qdrant_query(query_text, limit=10):
+    results = QDRANT_CLIENT.query(
+        collection_name=COLLECTION_NAME, query_text=query_text, limit=limit
+    )
+    logging.info(f"Results found for the query '{query_text}':\n{results}")
     return results
 
 
-def extract_data(repo):
-    extracted_data = []
-    for commit in repo.iter_commits():
-        commit_data = {
-            "ids": commit.hexsha,
-            "documents": str(
-                {
-                    "author": commit.author.name,
-                    "email": commit.author.email,
-                    "datetime": commit.committed_datetime.isoformat(),
-                    "message": commit.message,
-                    "diff": ", ".join([diff.a_path for diff in commit.diff(None)]),
-                    "branch": repo.active_branch.name
-                    if not repo.head.is_detached
-                    else "detached",
-                }
-            ),
-        }
-        extracted_data.append(commit_data)
+def gradio_chat(message, history):
+    for key, value in message.items():
+        if key == "text":
+            question = value
+        if key == "files":
+            for item in value:
+                file = DOC_CONVERTER.convert(item)
+                documents, metadatas = [], []
+                for chunk in HybridChunker().chunk(file.document):
+                    documents.append(chunk.text)
+                    metadatas.append(chunk.meta.export_json_dict())
+                qdrant_store(documents, metadatas)
 
-    logging.info(f"{len(extracted_data)} extracted commits from the repository.")
-    return extracted_data
+    # Get Context
+    points = qdrant_query(question)
+    for index, point in enumerate(points):
+        chat_history.append(
+            {"role": "user", "content": f"Context {index + 1}: {point}"}
+        )
 
-
-def chat_interface(message, history):
-    # Query Collection
-    context = query_chromadb(collection, message)
-
-    # Ask Question
-    user = f"The question is '{message}'. Here is all the context you have: {context}"
+    # Create Question
+    user = f"Question: '{question}'."
     chat_history.append({"role": "user", "content": user})
 
-    # Create Response
-    response = create_response(chat_history)
+    # Get Response
+    response = ollama_chat(chat_history)
     logging.info(f"Response: {response}")
-    chat_history.append({"role": "system", "content": response})
+    chat_history.append({"role": "assistant", "content": response})
 
     return response
 
 
 if __name__ == "__main__":
-    # Extract Repository Data
-    url = ""
-    assert url, "Please provide a repository url."
-    repo = clone_repository(url)
-
-    # Initialize ChromaDB
-    chroma_client = chromadb.PersistentClient(
-        path="chroma.db", settings=Settings(anonymized_telemetry=False)
+    # Initialize Ollama and Qdrant Clients
+    OLLAMA_CLIENT = Client()
+    QDRANT_CLIENT = QdrantClient()
+    QDRANT_CLIENT.set_model("sentence-transformers/all-MiniLM-L6-v2")
+    QDRANT_CLIENT.set_sparse_model("Qdrant/bm25")
+    DOC_CONVERTER = DocumentConverter(
+        allowed_formats=[
+            InputFormat.PDF,
+            InputFormat.DOCX,
+            InputFormat.HTML,
+            InputFormat.PPTX,
+            InputFormat.ASCIIDOC,
+            InputFormat.CSV,
+            InputFormat.MD,
+        ],
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend
+            ),
+            InputFormat.DOCX: WordFormatOption(
+                pipeline_cls=SimplePipeline, backend=MsWordDocumentBackend
+            ),
+        },
     )
-    collection = chroma_client.get_or_create_collection(name="repository")
 
-    # Load and Insert Documents
-    documents = extract_data(repo)
-    store_in_chromadb(collection, documents)
+    # Create Qdrant Collection
+    COLLECTION_NAME = "docling"
+    if QDRANT_CLIENT.collection_exists(COLLECTION_NAME):
+        QDRANT_CLIENT.delete_collection(COLLECTION_NAME)
+
+    if not QDRANT_CLIENT.collection_exists(COLLECTION_NAME):
+        QDRANT_CLIENT.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config=QDRANT_CLIENT.get_fastembed_vector_params(),
+            sparse_vectors_config=QDRANT_CLIENT.get_fastembed_sparse_vector_params(),
+        )
 
     # Enrich Chat History
     chat_history = []
-    assistant = """I am going to ask you a question, which I would like you to answer
+    system = """I am going to ask you a question, which I would like you to answer
         based only on the provided context, and not any other information.
         If there is not enough information in the context to answer the question,
         say "I am not sure", then try to make a guess.
         Break your answer up into nicely readable paragraphs."""
-    chat_history.append({"role": "system", "content": assistant})
+    chat_history.append({"role": "system", "content": system})
 
     gr.ChatInterface(
-        fn=chat_interface, type="messages", title="Chatbot", save_history=True
+        fn=gradio_chat,
+        type="messages",
+        multimodal=True,
+        textbox=gr.MultimodalTextbox(
+            file_count="single",
+            file_types=[".pdf", ".docx", ".html", ".pptx", ".asciidoc", ".csv", ".md"],
+        ),
+        save_history=True,
+        theme="base",
     ).launch()
